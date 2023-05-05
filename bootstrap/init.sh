01f2a1d866953f9d1d2b5dea2d0d964de1ba6162
dan-os <48282179+dan-os@users.noreply.github.com>
2023-05-05 17:54:28 +0100

    Update bootstrap script

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,16 +1,22 @@
 #!/bin/vbash
 
+# # Generate key for GitHub
+# ssh-keygen -t ed25519 -C "vyos@dan.sm" -f /config/secrets/id_ed25519
+
+# # Generate age key
+# age-keygen -o /config/secrets/vyos.agekey
+
 source /opt/vyatta/etc/functions/script-template
 
 load /opt/vyatta/etc/config.boot.default
 
-# Generate key for GitHub
-ssh-keygen -t ed25519 -C "vyos@dan.sm" -f /config/secrets/id_ed25519
-
-# Generate age key
-age-keygen -o /config/secrets/vyos.agekey
-
-# SSH
+# Interface
 set interfaces ethernet eth0 description 'WAN'
 set interfaces ethernet eth0 address 'dhcp'
+set system name-server '1.1.1.1'
+
+# SSH
 set service ssh port '22'
+set system login user vyos authentication public-keys dan-macbook key 'AAAAC3NzaC1lZDI1NTE5AAAAIDxJWUR/LYvaS7H2fVp/UX/xPo4JLdQRxSBkH3wZ4Bcl'
+set system login user vyos authentication public-keys dan-macbook type 'ssh-ed25519'
+
